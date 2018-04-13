d86e5c9adf38709c10767969e738b4f120b38512
nineclown <oring315@gmail.com>
2018-04-13 13:11:23 +0900

    gdgd

--- src/test/java/com/test/tutorial/serenity/models/SiteArea.java
+++
@@ -1,5 +0,0 @@
-package com.test.tutorial.serenity.models;
-
-public enum SiteArea {
-    Overlord
-}
--- src/test/java/com/test/tutorial/serenity/ui/CurrentPage.java
+++
@@ -1,7 +0,0 @@
-package com.test.tutorial.serenity.ui;
-
-import net.serenitybdd.core.pages.PageObject;
-
-public class CurrentPage extends PageObject {
-
-}
